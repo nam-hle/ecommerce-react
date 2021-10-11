e4b0ba0976606cdb38387c194d8d7de5804c970f
Nam Hoang Le <nam.hoang.le@mgm-tp.com>
2021-10-11 08:28:03 +0700

    Type for product actions

--- src/redux/actions/productActions.ts
+++ src/redux/actions/productActions.ts
@@ -12,25 +12,38 @@ import {
   SEARCH_PRODUCT,
   SEARCH_PRODUCT_SUCCESS,
 } from "../../constants";
+import { Product, SearchedProduct } from "../reducers/productReducer";
+
+export type GetProductsPayload = string;
 
 export const getProducts = (lastRef: string) => ({
   type: GET_PRODUCTS,
   payload: lastRef,
 });
 
-export const getProductsSuccess = (products: string) => ({
+export type GetProductsSuccessPayload = SearchedProduct;
+
+export const getProductsSuccess = (products: Product) => ({
   type: GET_PRODUCTS_SUCCESS,
   payload: products,
 });
+
+export interface CancelGetProductsPayload {}
 
 export const cancelGetProducts = () => ({
   type: CANCEL_GET_PRODUCTS,
 });
 
-export const addProduct = (product: string) => ({
+export type AddProductPayload = Product;
+
+export const addProduct = (product: Product) => ({
   type: ADD_PRODUCT,
   payload: product,
 });
+
+export interface SearchProductPayload {
+  searchKey: string;
+}
 
 export const searchProduct = (searchKey: string) => ({
   type: SEARCH_PRODUCT,
@@ -39,29 +52,44 @@ export const searchProduct = (searchKey: string) => ({
   },
 });
 
+export type SearchProductSuccessPayload = SearchedProduct;
+
 export const searchProductSuccess = (products: string) => ({
   type: SEARCH_PRODUCT_SUCCESS,
   payload: products,
 });
 
+export interface ClearSearchStatePayload {}
+
 export const clearSearchState = () => ({
   type: CLEAR_SEARCH_STATE,
 });
+
+export type AddProductSuccessPayload = Product;
 
 export const addProductSuccess = (product: string) => ({
   type: ADD_PRODUCT_SUCCESS,
   payload: product,
 });
 
+export type RemoveProductPayload = string;
+
 export const removeProduct = (id: string) => ({
   type: REMOVE_PRODUCT,
   payload: id,
 });
 
+export type RemoveProductSuccessPayload = string;
+
 export const removeProductSuccess = (id: string) => ({
   type: REMOVE_PRODUCT_SUCCESS,
   payload: id,
 });
+
+export interface EditProductPayload {
+  id: string;
+  updates: any;
+}
 
 export const editProduct = (id: string, updates: string) => ({
   type: EDIT_PRODUCT,
@@ -70,6 +98,11 @@ export const editProduct = (id: string, updates: string) => ({
     updates,
   },
 });
+
+export interface EditProductSuccessPayload {
+  id: string;
+  updates: any;
+}
 
 export const editProductSuccess = (updates: string) => ({
   type: EDIT_PRODUCT_SUCCESS,
---
+++ src/redux/reducers/productReducer.ts
@@ -0,0 +1,112 @@
+import {
+  ADD_PRODUCT_SUCCESS,
+  CLEAR_SEARCH_STATE,
+  EDIT_PRODUCT_SUCCESS,
+  GET_PRODUCTS_SUCCESS,
+  REMOVE_PRODUCT_SUCCESS,
+  SEARCH_PRODUCT_SUCCESS,
+} from "../../constants";
+import {
+  AddProductSuccessPayload,
+  ClearSearchStatePayload,
+  EditProductSuccessPayload,
+  RemoveProductSuccessPayload,
+  SearchProductSuccessPayload,
+} from "../actions/productActions";
+import { GetProductsSuccessPayload } from "../actions/productActions";
+import { ActionWithPayload } from "./index";
+
+export interface Product {
+  id: string;
+}
+
+export interface SearchedProduct {
+  lastRefKey: string | null;
+  total: number;
+  items: Product[];
+}
+
+export interface ProductState extends SearchedProduct {
+  searchedProducts: SearchedProduct;
+}
+
+export type GetProductsSuccessAction = ActionWithPayload<typeof GET_PRODUCTS_SUCCESS, GetProductsSuccessPayload>;
+export type AddProductSuccessAction = ActionWithPayload<typeof ADD_PRODUCT_SUCCESS, AddProductSuccessPayload>;
+export type EditProductSuccessAction = ActionWithPayload<typeof EDIT_PRODUCT_SUCCESS, EditProductSuccessPayload>;
+export type RemoveProductSuccessAction = ActionWithPayload<typeof REMOVE_PRODUCT_SUCCESS, RemoveProductSuccessPayload>;
+
+export type SearchProductSuccessAction = ActionWithPayload<typeof SEARCH_PRODUCT_SUCCESS, SearchProductSuccessPayload>;
+export type ClearSearchStateAction = ActionWithPayload<typeof CLEAR_SEARCH_STATE, ClearSearchStatePayload>;
+
+export type ProductAction =
+  | GetProductsSuccessAction
+  | AddProductSuccessAction
+  | RemoveProductSuccessAction
+  | EditProductSuccessAction
+  | SearchProductSuccessAction
+  | ClearSearchStateAction;
+
+const initState: SearchedProduct = {
+  lastRefKey: null,
+  total: 0,
+  items: [],
+};
+
+export default (
+  state: ProductState = {
+    lastRefKey: null,
+    total: 0,
+    items: [],
+    searchedProducts: initState,
+  },
+  action: ProductAction
+) => {
+  switch (action.type) {
+    case GET_PRODUCTS_SUCCESS:
+      return {
+        ...state,
+        lastRefKey: action.payload.lastRefKey,
+        total: action.payload.total,
+        items: [...state.items, ...action.payload.items],
+      };
+    case ADD_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        items: [...state.items, action.payload],
+      };
+    case SEARCH_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        searchedProducts: {
+          lastRefKey: action.payload.lastRefKey,
+          total: action.payload.total,
+          items: [...state.searchedProducts.items, ...action.payload.items],
+        },
+      };
+    case CLEAR_SEARCH_STATE:
+      return {
+        ...state,
+        searchedProducts: initState,
+      };
+    case REMOVE_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        items: state.items.filter((product) => product.id !== action.payload),
+      };
+    case EDIT_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        items: state.items.map((product) => {
+          if (product.id === action.payload.id) {
+            return {
+              ...product,
+              ...action.payload.updates,
+            };
+          }
+          return product;
+        }),
+      };
+    default:
+      return state;
+  }
+};
